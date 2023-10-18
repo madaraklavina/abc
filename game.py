4d0dfd3f24cbce634d7653a8ff7ad1abc47b8afd
madaraklavina <148310215+madaraklavina@users.noreply.github.com>
2023-10-18 08:35:12 +0300

    first version of the game

---
+++ game.py
@@ -0,0 +1,31 @@
+import random
+
+repeat = True
+
+while repeat:
+    number = random.randint(1, 100)
+    guess = 0
+    tries = 0
+
+    while guess != number:
+        if guess > number:
+            print("mazāk")
+        else:
+            print("vairāk")
+
+        guess = int(input("Uzmini skaitli: "))
+        tries += 1
+    else:
+        if tries < 4:
+            print(f"tu uzminēji tikai pēc {tries} reizēm")
+        elif tries < 7:
+            print(f"uzmineji pēc {tries} reizem")
+        else:
+            print("hmm tomer uzminēji")   
+
+    response = input("vai gribi turpināt? y/n: ")
+    if response == "y":
+        repeat = True
+    else:
+        repeat = False
+        print("paldies par speli")
