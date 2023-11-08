923d8b3e2d3a97c7d8e8359a4e8c87c5b4851dd6
madaraklavina <148310215+madaraklavina@users.noreply.github.com>
2023-11-08 08:19:51 +0200

    Create lists_method.py

---
+++ lists_method.py
@@ -0,0 +1,13 @@
+```py
+shopping_list = ['milk', 'avokado']
+shopping_list.extend(['bananas', 'chocolate', 'nuts'])
+print(shopping_list)
+
+item_to_delete = input("kuru elementu dzēst?")
+
+if item_to_delete in shopping_list:
+    shopping_list.remove(item_to_delete)
+else:
+    print("item not in list")
+print(shopping_list)
+```
